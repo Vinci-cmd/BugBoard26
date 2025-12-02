050fba3714451c5c361a9be3693a1fe17817b2fd
simone <simone.ascione@gmail.com>
2025-12-02 15:41:55 +0100

    fix vari

--- backend/src/main/java/com/bugboard/model/Issue.java
+++ backend/src/main/java/com/bugboard/model/Issue.java
@@ -1,7 +1,7 @@
 package com.bugboard.model;
 
 import jakarta.persistence.*;
-import java.util.Date; // UML dice Date
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -10,7 +10,7 @@ public class Issue {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id; // UML: id: int
+    private Integer id;
 
     @Column(nullable = false)
     private String titolo;
@@ -22,6 +22,7 @@ public class Issue {
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCreazione;
 
+    // Campo legacy per percorso file (opzionale se usi BLOB, ma lo lascio per sicurezza)
     @Column(name = "percorso_allegato")
     private String percorsoAllegato;
 
@@ -36,64 +37,125 @@ public class Issue {
     @Enumerated(EnumType.STRING)
     private IssuePriority priorita;
 
-    // Relazione dal diagramma: User "1" --> "0..*" Issue : crea
+    // --- GESTIONE ALLEGATO (BLOB) ---
+    
+    @Lob 
+    @Column(name = "allegato", columnDefinition = "BYTEA") 
+    private byte[] allegato; 
+
+    @Column(name = "nome_file_allegato")
+    private String nomeFileAllegato;
+
+    // --- RELAZIONI ---
+
     @ManyToOne
     @JoinColumn(name = "autore_id", nullable = false)
     private User autore;
 
-    // Relazione dal diagramma: Issue "1" *-- "0..*" Comment : contiene
-    // (Cascade ALL perché è una composizione forte nel diagramma)
     @OneToMany(mappedBy = "issue", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
-@Lob // Specifica che è un oggetto grande (Large Object)
-    @Column(name = "allegato", columnDefinition = "LONGBLOB") // Usa "BYTEA" se usi PostgreSQL, "LONGBLOB" per MySQL/H2
-    private byte[] allegato; 
 
-    // AGGIUNGI ANCHE QUESTO (utile per il frontend):
-    @Column(name = "nome_file_allegato")
-    private String nomeFileAllegato;
+    // --- COSTRUTTORI ---
 
-    // ... getter e setter per i nuovi campi ...
-    public byte[] getAllegato() { return allegato; }
-    public void setAllegato(byte[] allegato) { this.allegato = allegato; }
-
-    public String getNomeFileAllegato() { return nomeFileAllegato; }
-    public void setNomeFileAllegato(String nomeFileAllegato) { this.nomeFileAllegato = nomeFileAllegato; }
-    
     public Issue() {
-        this.dataCreazione = new Date(); // Imposta data corrente
+        this.dataCreazione = new Date();
     }
 
-    // --- Metodi del Diagramma UML ---
+    // --- GETTERS E SETTERS ---
 
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getTitolo() {
         return titolo;
     }
 
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Date getDataCreazione() {
+        return dataCreazione;
+    }
+
+    public void setDataCreazione(Date dataCreazione) {
+        this.dataCreazione = dataCreazione;
+    }
+
     public IssueStatus getStato() {
         return stato;
     }
 
-    // --- Altri Getter e Setter standard ---
-    public void setId(Integer id) { this.id = id; }
-    public void setTitolo(String titolo) { this.titolo = titolo; }
-    public String getDescrizione() { return descrizione; }
-    public void setDescrizione(String descrizione) { this.descrizione = descrizione; }
-    public Date getDataCreazione() { return dataCreazione; }
-    public void setDataCreazione(Date dataCreazione) { this.dataCreazione = dataCreazione; }
-    public String getPercorsoAllegato() { return percorsoAllegato; }
-    public void setPercorsoAllegato(String percorsoAllegato) { this.percorsoAllegato = percorsoAllegato; }
-    public void setStato(IssueStatus stato) { this.stato = stato; }
-    public IssueType getTipo() { return tipo; }
-    public void setTipo(IssueType tipo) { this.tipo = tipo; }
-    public IssuePriority getPriorita() { return priorita; }
-    public void setPriorita(IssuePriority priorita) { this.priorita = priorita; }
-    public User getAutore() { return autore; }
-    public void setAutore(User autore) { this.autore = autore; }
-    public List<Comment> getComments() { return comments; }
-    public void setComments(List<Comment> comments) { this.comments = comments; }
+    public void setStato(IssueStatus stato) {
+        this.stato = stato;
+    }
+
+    public IssueType getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(IssueType tipo) {
+        this.tipo = tipo;
+    }
+
+    public IssuePriority getPriorita() {
+        return priorita;
+    }
+
+    public void setPriorita(IssuePriority priorita) {
+        this.priorita = priorita;
+    }
+
+    public User getAutore() {
+        return autore;
+    }
+
+    public void setAutore(User autore) {
+        this.autore = autore;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+    
+    public byte[] getAllegato() {
+        return allegato;
+    }
+
+    public void setAllegato(byte[] allegato) {
+        this.allegato = allegato;
+    }
+
+    public String getNomeFileAllegato() {
+        return nomeFileAllegato;
+    }
+
+    public void setNomeFileAllegato(String nomeFileAllegato) {
+        this.nomeFileAllegato = nomeFileAllegato;
+    }
+
+    public String getPercorsoAllegato() {
+        return percorsoAllegato;
+    }
+
+    public void setPercorsoAllegato(String percorsoAllegato) {
+        this.percorsoAllegato = percorsoAllegato;
+    }
 }
